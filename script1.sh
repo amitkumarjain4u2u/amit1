b52ea188309a6df781e4394ef5f30d8b9fc7c732
root <root@localhost.localdomain>
2019-10-20 13:52:50 +0000

    script file commiting

---
+++ script1.sh
@@ -0,0 +1,133 @@
+mandb
+mandb
+mandb
+yum update -y
+yum install epel-release -y
+yum -y update
+yum -y install mlocate
+updatedb
+yum install repolist all -y
+yum install repolist -y
+yum repolist all -y
+yum install java -y
+yum install perl -y
+yum install curl -y
+yum install jenkins -y
+yum install python -y
+#sudo yum remove docker \
+ #                 docker-client \
+  #                docker-client-latest \
+   #               docker-common \
+    #              docker-latest \
+     #             docker-latest-logrotate \
+      #            docker-logrotate \
+       #           docker-engine -y
+#
+yum install docker -y
+yum install yaml -y
+yum install httpd -y
+yum install sshd -y
+yum install ruby -y
+yum install vim -y
+yum install zip -y
+yum install gunzip -y
+yum install tar -y
+yum install unzip -y
+yum install cpp -y
+yum install langpacks -y
+yum install wget -y
+yum install zip -y
+yum install tar -y
+yum install gunzip -y
+yum install gzip -y
+yum install unzip -y
+yum install ansible -y
+yum install wireshark -y
+yum install gedit -y
+yum install gpedit -y
+yum install edit -y
+yum install get -y
+yum groupinstall -y
+yum install fontconfig freetype urw-fonts yum-langpacks -y
+yum install tree -y
+yum install info -y
+yum install pinfo -y
+yum install man -y
+yum install langpacks* -y
+yum install langpacks-* -y
+yum list langpacks-*
+yum groupinstall Fonts -y
+yum install grouplist -y
+yum install fonts -y
+yum install git -y
+yum install github -y
+yum install gitlab -y
+yum install maven -y
+yum install java -y
+yum install jdk -y
+yum install jre -y
+yum install mysql -y
+yum install mariadb -y
+yum install postgresql -y
+yum install apt-get -y
+yum install apt -y
+yum install get -y
+yum install gnome -y
+yum install apache -y
+yum install apache2 -y
+yum install vagrant -y
+yum instal jenkins -y
+yum install jenkins -y
+yum install dnf -y
+yum install rpm -y
+yum install pam -y
+yum install pem -y
+yum install ftp -y
+yum install sftp -y
+yum install tftp -y
+yum install filezilla
+yum install filezilla -y
+yum install winscp -y
+yum install putty -y
+yum install deb -y
+yum install debian -y
+yum install mozilla -y
+yum install firefox -y
+yum install chrome -y
+yum install google-chrome -y
+yum install sql -y
+yum install xml -y
+yum install software -y
+yum install soft -y
+yum install mysql-server -y
+yum install virt-manager -y
+yum install virsh virt-manager -y
+yum install virsh kvm virt-manager -y
+yum install virt-manager -y
+yum install vnstat -y
+yum install iftop -y
+yum install nload -y
+yum install nethogs -y
+yum install bmon -y
+yum install darkstat -y
+yum install iptraf -y
+yum install cbm -y
+yum install iperf3 -y
+yum install Netperf -y
+yum install netperf -y
+yum install sarg -y
+yum install monitorix -y
+yum install cacti -y
+yum install observium -y
+yum install zabbix -y
+yum install nagios -y
+yum install terraform -y
+yum install php -y
+yum install ldap -y
+yum install smtp -y
+yum install tracert -y
+yum install ping -y
+yum install gnome -y
+yum install notepadplusplus -y
+yum install notepad-plus-plus -y
+
