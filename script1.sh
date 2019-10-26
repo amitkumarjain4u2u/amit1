80644f0190412e4b50e6bc602d44d28cbfeb8db3
amitkumarjain4u2u <amitkumarjain4u2u@gmail.com>
2019-10-27 00:00:07 +0530

    Update script1.sh
    Commiting changes

--- script1.sh
+++ script1.sh
@@ -820,3 +820,414 @@ yum upgrade wonderwidgets -y
 yum upgrade rhcsa-script -y
 yum upgrade rhcsa-script -y
 yum upgrade guile -y
+mandb
+mandb
+mandb
+yum upgrade -y
+yum upgrade epel-release -y
+yum -y upgrade
+yum -y upgrade mlocate
+upgradedb
+yum upgrade repolist all -y
+yum upgrade repolist -y
+yum repolist all -y
+yum upgrade java -y
+yum upgrade perl -y
+yum upgrade curl -y
+yum upgrade jenkins -y
+yum upgrade python -y
+#sudo yum remove docker \
+ #                 docker-client \
+  #                docker-client-latest \
+   #               docker-common \
+    #              docker-latest \
+     #             docker-latest-logrotate \
+      #            docker-logrotate \
+       #           docker-engine -y
+#
+yum upgrade docker -y
+yum upgrade yaml -y
+yum upgrade httpd -y
+yum upgrade sshd -y
+yum upgrade ruby -y
+yum upgrade vim -y
+yum upgrade zip -y
+yum upgrade gunzip -y
+yum upgrade tar -y
+yum upgrade unzip -y
+yum upgrade cpp -y
+yum upgrade langpacks -y
+yum upgrade wget -y
+yum upgrade zip -y
+yum upgrade tar -y
+yum upgrade gunzip -y
+yum upgrade gzip -y
+yum upgrade unzip -y
+yum upgrade ansible -y
+yum upgrade wireshark -y
+yum upgrade gedit -y
+yum upgrade gpedit -y
+yum upgrade edit -y
+yum upgrade get -y
+yum groupupgrade -y
+yum upgrade fontconfig freetype urw-fonts yum-langpacks -y
+yum upgrade tree -y
+yum upgrade info -y
+yum upgrade pinfo -y
+yum upgrade man -y
+yum upgrade langpacks -y
+yum upgrade langpacks- -y
+yum list langpacks-
+yum groupupgrade Fonts -y
+yum upgrade grouplist -y
+yum upgrade fonts -y
+yum upgrade git -y
+yum upgrade github -y
+yum upgrade gitlab -y
+yum upgrade maven -y
+yum upgrade java -y
+yum upgrade jdk -y
+yum upgrade jre -y
+yum upgrade mysql -y
+yum upgrade mariadb -y
+yum upgrade postgresql -y
+yum upgrade apt-get -y
+yum upgrade apt -y
+yum upgrade get -y
+yum upgrade gnome -y
+yum upgrade apache -y
+yum upgrade apache2 -y
+yum upgrade vagrant -y
+yum instal jenkins -y
+yum upgrade jenkins -y
+yum upgrade dnf -y
+yum upgrade rpm -y
+yum upgrade pam -y
+yum upgrade pem -y
+yum upgrade ftp -y
+yum upgrade sftp -y
+yum upgrade tftp -y
+yum upgrade filezilla
+yum upgrade filezilla -y
+yum upgrade winscp -y
+yum upgrade putty -y
+yum upgrade deb -y
+yum upgrade debian -y
+yum upgrade mozilla -y
+yum upgrade firefox -y
+yum upgrade chrome -y
+yum upgrade google-chrome -y
+yum upgrade sql -y
+yum upgrade xml -y
+yum upgrade software -y
+yum upgrade soft -y
+yum upgrade mysql-server -y
+yum upgrade virt-manager -y
+yum upgrade virsh virt-manager -y
+yum upgrade virsh kvm virt-manager -y
+yum upgrade virt-manager -y
+yum upgrade vnstat -y
+yum upgrade iftop -y
+yum upgrade nload -y
+yum upgrade nethogs -y
+yum upgrade bmon -y
+yum upgrade darkstat -y
+yum upgrade iptraf -y
+yum upgrade cbm -y
+yum upgrade iperf3 -y
+yum upgrade Netperf -y
+yum upgrade netperf -y
+yum upgrade sarg -y
+yum upgrade monitorix -y
+yum upgrade cacti -y
+yum upgrade observium -y
+yum upgrade zabbix -y
+yum upgrade nagios -y
+yum upgrade terraform -y
+yum upgrade php -y
+yum upgrade ldap -y
+yum upgrade smtp -y
+yum upgrade tracert -y
+yum upgrade ping -y
+yum upgrade gnome -y
+yum upgrade notepadplusplus -y
+yum upgrade notepad-plus-plus -y
+yum upgrade gitlabs -y
+yum upgrade gnucash -y
+yum upgrade scribus -y
+yum upgrade projectlibre -y
+yum upgrade GIMP -y
+yum upgrade gimp -y
+yum upgrade gnotime -y
+yum upgrade lastpass -y
+yum upgrade xiki -y
+yum upgrade bluefish -y
+yum upgrade kexi -y
+yum upgrade zim -y
+yum upgrade focuswriter -y
+yum upgrade guake -y
+yum upgrade kritadesktop -y
+yum upgrade evolution -y
+yum upgrade cat -y
+yum upgrade zcat -y
+yum upgrade cal -y
+yum upgrade textedit -y
+yum upgrade sublime -y
+yum upgrade sublimetext -y
+yum upgrade banner -y
+yum upgrade svn -y
+yum upgrade toroise -y
+yum upgrade svntortoise -y
+yum upgrade vpn -y
+yum upgrade camel -y
+yum upgrade kafka -y
+yum upgrade istio -y
+yum upgrade kubernetes -y
+yum upgrade json -y
+yum upgrade postman -y
+yum upgrade soap -y
+yum upgrade soapui -y
+yum upgrade rest -y
+yum upgrade restapi -y
+yum upgrade api -y
+yum upgrade minikube -y
+yum upgrade minishift -y
+yum upgrade nginx -y
+yum upgrade snap -y
+yum upgrade SNAP -y
+yum upgrade ebtables ethtool -y
+yum upgrade ebtables -y
+yum upgrade ethtool -y
+yum upgrade docker.io -y
+yum upgrade docker -y
+systemctl enable docker && systemctl start docker -y
+yum upgrade kubelet kubeadm kubectl -y
+mandb
+mandb
+mandb
+yum upgrade -y
+yum upgrade epel-release -y
+yum -y upgrade
+yum -y upgrade mlocate
+upgradedb
+yum upgrade repolist all -y
+yum upgrade repolist -y
+yum repolist all -y
+yum upgrade java -y
+yum upgrade perl -y
+yum upgrade curl -y
+yum upgrade jenkins -y
+yum upgrade python -y
+#sudo yum remove docker \
+ #                 docker-client \
+  #                docker-client-latest \
+   #               docker-common \
+    #              docker-latest \
+     #             docker-latest-logrotate \
+      #            docker-logrotate \
+       #           docker-engine -y
+#
+yum upgrade docker -y
+yum upgrade yaml -y
+yum upgrade httpd -y
+yum upgrade sshd -y
+yum upgrade ruby -y
+yum upgrade vim -y
+yum upgrade zip -y
+yum upgrade gunzip -y
+yum upgrade tar -y
+yum upgrade unzip -y
+yum upgrade cpp -y
+yum upgrade langpacks -y
+yum upgrade wget -y
+yum upgrade zip -y
+yum upgrade tar -y
+yum upgrade gunzip -y
+yum upgrade gzip -y
+yum upgrade unzip -y
+yum upgrade ansible -y
+yum upgrade wireshark -y
+yum upgrade gedit -y
+yum upgrade gpedit -y
+yum upgrade edit -y
+yum upgrade get -y
+yum groupupgrade -y
+yum upgrade fontconfig freetype urw-fonts yum-langpacks -y
+yum upgrade tree -y
+yum upgrade info -y
+yum upgrade pinfo -y
+yum upgrade man -y
+yum upgrade langpacks -y
+yum upgrade langpacks- -y
+yum list langpacks-
+yum groupupgrade Fonts -y
+yum upgrade grouplist -y
+yum upgrade fonts -y
+yum upgrade git -y
+yum upgrade github -y
+yum upgrade gitlab -y
+yum upgrade maven -y
+yum upgrade java -y
+yum upgrade jdk -y
+yum upgrade jre -y
+yum upgrade mysql -y
+yum upgrade mariadb -y
+yum upgrade postgresql -y
+yum upgrade apt-get -y
+yum upgrade apt -y
+yum upgrade get -y
+yum upgrade gnome -y
+yum upgrade apache -y
+yum upgrade apache2 -y
+yum upgrade vagrant -y
+yum instal jenkins -y
+yum upgrade jenkins -y
+yum upgrade dnf -y
+yum upgrade rpm -y
+yum upgrade pam -y
+yum upgrade pem -y
+yum upgrade ftp -y
+yum upgrade sftp -y
+yum upgrade tftp -y
+yum upgrade filezilla
+yum upgrade filezilla -y
+yum upgrade winscp -y
+yum upgrade putty -y
+yum upgrade deb -y
+yum upgrade debian -y
+yum upgrade mozilla -y
+yum upgrade firefox -y
+yum upgrade chrome -y
+yum upgrade google-chrome -y
+yum upgrade sql -y
+yum upgrade xml -y
+yum upgrade software -y
+yum upgrade soft -y
+yum upgrade mysql-server -y
+yum upgrade virt-manager -y
+yum upgrade virsh virt-manager -y
+yum upgrade virsh kvm virt-manager -y
+yum upgrade virt-manager -y
+yum upgrade vnstat -y
+yum upgrade iftop -y
+yum upgrade nload -y
+yum upgrade nethogs -y
+yum upgrade bmon -y
+yum upgrade darkstat -y
+yum upgrade iptraf -y
+yum upgrade cbm -y
+yum upgrade iperf3 -y
+yum upgrade Netperf -y
+yum upgrade netperf -y
+yum upgrade sarg -y
+yum upgrade monitorix -y
+yum upgrade cacti -y
+yum upgrade observium -y
+yum upgrade zabbix -y
+yum upgrade nagios -y
+yum upgrade terraform -y
+yum upgrade php -y
+yum upgrade ldap -y
+yum upgrade smtp -y
+yum upgrade tracert -y
+yum upgrade ping -y
+yum upgrade gnome -y
+yum upgrade notepadplusplus -y
+yum upgrade notepad-plus-plus -y
+yum upgrade gitlabs -y
+yum upgrade gnucash -y
+yum upgrade scribus -y
+yum upgrade projectlibre -y
+yum upgrade GIMP -y
+yum upgrade gimp -y
+yum upgrade gnotime -y
+yum upgrade lastpass -y
+yum upgrade xiki -y
+yum upgrade bluefish -y
+yum upgrade kexi -y
+yum upgrade zim -y
+yum upgrade focuswriter -y
+yum upgrade guake -y
+yum upgrade kritadesktop -y
+yum upgrade evolution -y
+yum upgrade cat -y
+yum upgrade zcat -y
+yum upgrade cal -y
+yum upgrade textedit -y
+yum upgrade sublime -y
+yum upgrade sublimetext -y
+yum upgrade banner -y
+yum upgrade svn -y
+yum upgrade toroise -y
+yum upgrade svntortoise -y
+yum upgrade vpn -y
+yum upgrade camel -y
+yum upgrade kafka -y
+yum upgrade istio -y
+yum upgrade kubernetes -y
+yum upgrade json -y
+yum upgrade postman -y
+yum upgrade soap -y
+yum upgrade soapui -y
+yum upgrade rest -y
+yum upgrade restapi -y
+yum upgrade api -y
+yum upgrade minikube -y
+yum upgrade minishift -y
+yum upgrade nginx -y
+yum upgrade snap -y
+yum upgrade SNAP -y
+yum upgrade ebtables ethtool -y
+yum upgrade ebtables -y
+yum upgrade ethtool -y
+yum upgrade docker.io -y
+yum upgrade docker -y
+systemctl enable docker && systemctl start docker -y
+yum upgrade kubelet kubeadm kubectl -y
+yum upgrade createrepo -y
+yum upgrade ss -y
+yum upgrade netstat -y
+yum upgrade net -y
+yum upgrade chrony -y
+yum upgrade hist -y
+systemctl restart chronyd.service
+systemctl status chronyd.service
+yum upgrade ntp -y
+yum upgrade http -y
+yum upgrade deb -y
+yum upgrade msi -y
+yum upgrade rpm -y
+yum upgrade query -y
+yum upgrade build -y
+yum upgrade wis -y
+yum upgrade apt -y
+yum upgrade dnf -y
+yum upgrade coreutils -y
+yum upgrade vsftpd -y
+yum upgrade tree -y
+yum upgrade dir -y
+yum upgrade cpio -y
+yum upgrade create -y
+yum upgrade epel -y
+yum list available | wc -l
+yum list upgradeed | wc -l
+yum list all | wc -l
+yum list available
+yum list upgradeed
+yum list all
+yum groupupgrade "Virtualization Host" -y
+yum groupupgrade "Development Tools" -y
+yum groupupgrade "Security Tools" -y
+yum groupupgrade "System Administration Tools" -y
+yum groupupgrade "System Management" -y
+yum groupupgrade "Xfce" -y
+yum groupupgrade "Xfce" -y
+yum upgrade -y
+yum upgrade -y
+yum upgrade rpm2cpio -y
+yum upgrade rpm2cpio -y
+yum upgrade wonderwidgets -y
+yum upgrade wonderwidgets -y
+yum upgrade rhcsa-script -y
+yum upgrade rhcsa-script -y
+yum upgrade guile -y
