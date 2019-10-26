bf14897f5c603e285580ec27d255b4bec09387b6
amitkumarjain4u2u <amitkumarjain4u2u@gmail.com>
2019-10-26 23:14:46 +0530

    Update script1.sh
    Commiting changes

--- script1.sh
+++ script1.sh
@@ -53,9 +53,9 @@ yum install tree -y
 yum install info -y
 yum install pinfo -y
 yum install man -y
-yum install langpacks* -y
-yum install langpacks-* -y
-yum list langpacks-*
+yum install langpacks -y
+yum install langpacks- -y
+yum list langpacks-
 yum groupinstall Fonts -y
 yum install grouplist -y
 yum install fonts -y
@@ -183,19 +183,19 @@ yum install kubelet kubeadm kubectl -y
 mandb
 mandb
 mandb
-yum update* -y
-yum install epel-release* -y
+yum update -y
+yum install epel-release -y
 yum -y update
 yum -y install mlocate
 updatedb
-yum install repolist all* -y
-yum install repolist* -y
-yum repolist all* -y
+yum install repolist all -y
+yum install repolist -y
+yum repolist all -y
 yum install java -y
-yum install perl* -y
-yum install curl* -y
-yum install jenkins* -y
-yum install python* -y
+yum install perl -y
+yum install curl -y
+yum install jenkins -y
+yum install python -y
 #sudo yum remove docker \
  #                 docker-client \
   #                docker-client-latest \
@@ -203,190 +203,190 @@ yum install python* -y
     #              docker-latest \
      #             docker-latest-logrotate \
       #            docker-logrotate \
-       #           docker-engine* -y
+       #           docker-engine -y
 #
-yum install docker* -y
-yum install yaml* -y
-yum install httpd* -y
-yum install sshd* -y
-yum install ruby* -y
-yum install vim* -y
-yum install zip* -y
-yum install gunzip* -y
-yum install tar* -y
-yum install unzip* -y
-yum install cpp* -y
-yum install langpacks* -y
-yum install wget* -y
-yum install zip* -y
-yum install tar* -y
-yum install gunzip* -y
-yum install gzip* -y
-yum install unzip* -y
-yum install ansible* -y
-yum install wireshark* -y
-yum install gedit* -y
-yum install gpedit* -y
-yum install edit* -y
-yum install get* -y
-yum groupinstall* -y
-yum install fontconfig freetype urw-fonts yum-langpacks* -y
-yum install tree* -y
-yum install info* -y
-yum install pinfo* -y
-yum install man* -y
-yum install langpacks** -y
-yum install langpacks-** -y
-yum list langpacks-*
-yum groupinstall Fonts* -y
-yum install grouplist* -y
-yum install fonts* -y
-yum install git* -y
-yum install github* -y
-yum install gitlab* -y
-yum install maven* -y
-yum install java* -y
-yum install jdk* -y
-yum install jre* -y
-yum install mysql* -y
-yum install mariadb* -y
-yum install postgresql* -y
-yum install apt-get* -y
-yum install apt* -y
-yum install get* -y
-yum install gnome* -y
-yum install apache* -y
-yum install apache2* -y
-yum install vagrant* -y
-yum instal jenkins* -y
-yum install jenkins* -y
-yum install dnf* -y
-yum install rpm* -y
-yum install pam* -y
-yum install pem* -y
-yum install ftp* -y
-yum install sftp* -y
-yum install tftp* -y
+yum install docker -y
+yum install yaml -y
+yum install httpd -y
+yum install sshd -y
+yum install ruby -y
+yum install vim -y
+yum install zip -y
+yum install gunzip -y
+yum install tar -y
+yum install unzip -y
+yum install cpp -y
+yum install langpacks -y
+yum install wget -y
+yum install zip -y
+yum install tar -y
+yum install gunzip -y
+yum install gzip -y
+yum install unzip -y
+yum install ansible -y
+yum install wireshark -y
+yum install gedit -y
+yum install gpedit -y
+yum install edit -y
+yum install get -y
+yum groupinstall -y
+yum install fontconfig freetype urw-fonts yum-langpacks -y
+yum install tree -y
+yum install info -y
+yum install pinfo -y
+yum install man -y
+yum install langpacks -y
+yum install langpacks- -y
+yum list langpacks-
+yum groupinstall Fonts -y
+yum install grouplist -y
+yum install fonts -y
+yum install git -y
+yum install github -y
+yum install gitlab -y
+yum install maven -y
+yum install java -y
+yum install jdk -y
+yum install jre -y
+yum install mysql -y
+yum install mariadb -y
+yum install postgresql -y
+yum install apt-get -y
+yum install apt -y
+yum install get -y
+yum install gnome -y
+yum install apache -y
+yum install apache2 -y
+yum install vagrant -y
+yum instal jenkins -y
+yum install jenkins -y
+yum install dnf -y
+yum install rpm -y
+yum install pam -y
+yum install pem -y
+yum install ftp -y
+yum install sftp -y
+yum install tftp -y
 yum install filezilla
-yum install filezilla* -y
-yum install winscp* -y
-yum install putty* -y
-yum install deb* -y
-yum install debian* -y
-yum install mozilla* -y
-yum install firefox* -y
-yum install chrome* -y
-yum install google-chrome* -y
-yum install sql* -y
-yum install xml* -y
-yum install software* -y
-yum install soft* -y
-yum install mysql-server* -y
-yum install virt-manager* -y
-yum install virsh virt-manager* -y
-yum install virsh kvm virt-manager* -y
-yum install virt-manager* -y
-yum install vnstat* -y
-yum install iftop* -y
-yum install nload* -y
-yum install nethogs* -y
-yum install bmon* -y
-yum install darkstat* -y
-yum install iptraf* -y
-yum install cbm* -y
-yum install iperf3* -y
-yum install Netperf* -y
-yum install netperf* -y
-yum install sarg* -y
-yum install monitorix* -y
-yum install cacti* -y
-yum install observium* -y
-yum install zabbix* -y
-yum install nagios* -y
-yum install terraform* -y
-yum install php* -y
-yum install ldap* -y
-yum install smtp* -y
-yum install tracert* -y
-yum install ping* -y
-yum install gnome* -y
-yum install notepadplusplus* -y
-yum install notepad-plus-plus* -y
-yum install gitlabs* -y
-yum install gnucash* -y
-yum install scribus* -y
-yum install projectlibre* -y
-yum install GIMP* -y
-yum install gimp* -y
-yum install gnotime* -y
-yum install lastpass* -y
-yum install xiki* -y
-yum install bluefish* -y
-yum install kexi* -y
-yum install zim* -y
-yum install focuswriter* -y
-yum install guake* -y
-yum install kritadesktop* -y
-yum install evolution* -y
-yum install cat* -y
-yum install zcat* -y
-yum install cal* -y
-yum install textedit* -y
-yum install sublime* -y
-yum install sublimetext* -y
-yum install banner* -y
-yum install svn* -y
-yum install toroise* -y
-yum install svntortoise* -y
-yum install vpn* -y
-yum install camel* -y
-yum install kafka* -y
-yum install istio* -y
-yum install kubernetes* -y
-yum install json* -y
-yum install postman* -y
-yum install soap* -y
-yum install soapui* -y
-yum install rest* -y
-yum install restapi* -y
-yum install api* -y
-yum install minikube* -y
-yum install minishift* -y
-yum install nginx* -y
-yum install snap* -y
-yum install SNAP* -y
-yum install ebtables ethtool* -y
-yum install ebtables* -y
-yum install ethtool* -y
-yum install docker.io* -y
-yum install docker* -y
-systemctl enable docker && systemctl start docker* -y
-yum install kubelet kubeadm kubectl* -y
-yum install createrepo* -y
-yum install ss* -y
-yum install netstat* -y
-yum install net* -y
-yum install chrony* -y
-yum install hist* -y
+yum install filezilla -y
+yum install winscp -y
+yum install putty -y
+yum install deb -y
+yum install debian -y
+yum install mozilla -y
+yum install firefox -y
+yum install chrome -y
+yum install google-chrome -y
+yum install sql -y
+yum install xml -y
+yum install software -y
+yum install soft -y
+yum install mysql-server -y
+yum install virt-manager -y
+yum install virsh virt-manager -y
+yum install virsh kvm virt-manager -y
+yum install virt-manager -y
+yum install vnstat -y
+yum install iftop -y
+yum install nload -y
+yum install nethogs -y
+yum install bmon -y
+yum install darkstat -y
+yum install iptraf -y
+yum install cbm -y
+yum install iperf3 -y
+yum install Netperf -y
+yum install netperf -y
+yum install sarg -y
+yum install monitorix -y
+yum install cacti -y
+yum install observium -y
+yum install zabbix -y
+yum install nagios -y
+yum install terraform -y
+yum install php -y
+yum install ldap -y
+yum install smtp -y
+yum install tracert -y
+yum install ping -y
+yum install gnome -y
+yum install notepadplusplus -y
+yum install notepad-plus-plus -y
+yum install gitlabs -y
+yum install gnucash -y
+yum install scribus -y
+yum install projectlibre -y
+yum install GIMP -y
+yum install gimp -y
+yum install gnotime -y
+yum install lastpass -y
+yum install xiki -y
+yum install bluefish -y
+yum install kexi -y
+yum install zim -y
+yum install focuswriter -y
+yum install guake -y
+yum install kritadesktop -y
+yum install evolution -y
+yum install cat -y
+yum install zcat -y
+yum install cal -y
+yum install textedit -y
+yum install sublime -y
+yum install sublimetext -y
+yum install banner -y
+yum install svn -y
+yum install toroise -y
+yum install svntortoise -y
+yum install vpn -y
+yum install camel -y
+yum install kafka -y
+yum install istio -y
+yum install kubernetes -y
+yum install json -y
+yum install postman -y
+yum install soap -y
+yum install soapui -y
+yum install rest -y
+yum install restapi -y
+yum install api -y
+yum install minikube -y
+yum install minishift -y
+yum install nginx -y
+yum install snap -y
+yum install SNAP -y
+yum install ebtables ethtool -y
+yum install ebtables -y
+yum install ethtool -y
+yum install docker.io -y
+yum install docker -y
+systemctl enable docker && systemctl start docker -y
+yum install kubelet kubeadm kubectl -y
+yum install createrepo -y
+yum install ss -y
+yum install netstat -y
+yum install net -y
+yum install chrony -y
+yum install hist -y
 systemctl restart chronyd.service
 systemctl status chronyd.service
-yum install ntp* -y
-yum install http* -y
-yum install deb* -y
-yum install msi* -y
-yum install rpm* -y
-yum install query* -y
-yum install build* -y
-yum install wis* -y
-yum install apt* -y
-yum install dnf* -y
-yum install coreutils* -y
-yum install vsftpd* -y
-yum install tree* -y
-yum install dir* -y
-yum install cpio* -y
-yum install create* -y
-yum install epel* -y
+yum install ntp -y
+yum install http -y
+yum install deb -y
+yum install msi -y
+yum install rpm -y
+yum install query -y
+yum install build -y
+yum install wis -y
+yum install apt -y
+yum install dnf -y
+yum install coreutils -y
+yum install vsftpd -y
+yum install tree -y
+yum install dir -y
+yum install cpio -y
+yum install create -y
+yum install epel -y
 yum list available | wc -l
 yum list installed | wc -l
 yum list all | wc -l
@@ -403,9 +403,8 @@ yum groupinstall "Xfce" -y
 yum upgrade -y
 yum update -y
 yum install rpm2cpio -y
-yum install rpm2cpio* -y
+yum install rpm2cpio -y
 yum install wonderwidgets -y
-yum install wonderwidgets* -y
+yum install wonderwidgets -y
 yum install rhcsa-script -y
-yum install rhcsa-script* -y
-
+yum install rhcsa-script -y
