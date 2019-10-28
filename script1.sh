5116b37e649f6407dea3548d72f1b89d27ea62b4
amitkumarjain4u2u <amitkumarjain4u2u@gmail.com>
2019-10-28 13:44:38 +0530

    Update script1.sh
    yum update -y
    yum install epel-release -y
    yum update -y
    yum install mlocate -y
    updatedb
    yum install repolist all -y
    yum install repolist -y
    yum repolist all -y
    yum install git -y

--- script1.sh
+++ script1.sh
@@ -9,6 +9,15 @@ updatedb
 yum install repolist all -y
 yum install repolist -y
 yum repolist all -y
+yum update -y
+yum install epel-release -y
+yum update -y
+yum install mlocate -y
+updatedb
+yum install repolist all -y
+yum install repolist -y
+yum repolist all -y
+yum install git -y
 yum install java -y
 yum install perl -y
 yum install curl -y
