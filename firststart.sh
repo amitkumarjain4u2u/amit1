9eb0de78f61828b496720b301455cc951b13a393
amitkumarjain4u2u <amitkumarjain4u2u@gmail.com>
2019-10-28 13:33:48 +0530

    Create firststart.sh
    yum update -y
    yum install epel-release -y
    yum update -y
    yum install mlocate -y
    updatedb
    yum install repolist all -y
    yum install repolist -y
    yum repolist all -y
    yum install git -y

---
+++ firststart.sh
@@ -0,0 +1,9 @@
+yum update -y
+yum install epel-release -y
+yum update -y
+yum install mlocate -y
+updatedb
+yum install repolist all -y
+yum install repolist -y
+yum repolist all -y
+yum install git -y
