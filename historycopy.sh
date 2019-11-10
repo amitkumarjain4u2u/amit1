ccc73b02507448847b7588bafdcec80045f7ad2f
amitkumarjain4u2u <amitkumarjain4u2u@gmail.com>
2019-11-10 21:41:31 +0000

    committing latest changes

--- historycopy.sh
+++ historycopy.sh
@@ -3,6 +3,13 @@ echo "Copying history command list - history command copy"
 history >> /root/amit1/amit1/his/history1.txt
 cat /root/amit1/amit1/his/history1.txt
 banner "THE END"
+echo "COPY PROCESS STARTED"
+echo "Copying history command list - history command copy"
+history >> /root/amit1/amit1/his/history1.txt
+cat /root/amit1/amit1/his/history1.txt
+banner "THE END"
+history -cw
+history -cw
 echo "HISTORY CLEANED"
 cat /root/amit1/amit1/his/history1.txt
 echo "COPY PROCESS COMPLETED"
@@ -13,5 +20,7 @@ history -c
 history
 history -c
 history -c
+history -cw
+history -cw
 history
 echo "HISTORY CLEANED SUCCESSFULLY"
